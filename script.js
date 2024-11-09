73902144503a16c02cdf7695ea81353c93448f3a
Kartik Ballal <125107204+KartikBallal1610@users.noreply.github.com>
2024-11-09 11:12:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to handle food ordering
 function orderFood() {
     let selectedItems = [];
     
@@ -28,19 +27,3 @@ function orderFood() {
         alert("Please select at least one item.");
     }
 }
-
-// Function to display ordered items on the Order Successful page
-window.onload = function() {
-    let orderItems = JSON.parse(localStorage.getItem("orderItems"));
-    let orderList = document.getElementById("orderList");
-
-    if (orderItems && orderItems.length > 0) {
-        orderItems.forEach(item => {
-            let li = document.createElement("li");
-            li.textContent = item;
-            orderList.appendChild(li);
-        });
-    } else {
-        orderList.innerHTML = "No items ordered.";
-    }
-};
